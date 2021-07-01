18b485a6de24eb53cd2c2c0fba2a3c089ebc73da
Sebastian F <sebpfave2@gmail.com>
2021-06-30 22:27:07 -0400

    Add base styling

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <ReactDOM.StrictMode>
+  <React.StrictMode>
     <App />
-  </ReactDOM.StrictMode>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
