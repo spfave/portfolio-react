f0b4e1913f44c814a0ea956da486f2cb70fb8240
Sebastian F <sebpfave2@gmail.com>
2021-07-02 16:46:11 -0400

    Demo Header and Footer component import

--- src/components/PortfolioSite.js
+++ src/components/PortfolioSite.js
@@ -1,3 +1,13 @@
+import { useState } from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
 export default function PortfolioSite() {
-  return <div>Sebastian Portfolio Site</div>;
+  return (
+    <div>
+      <Header />
+      <div>Sebastian Portfolio Site</div>
+      <Footer />
+    </div>
+  );
 }
