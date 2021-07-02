6718b688d99b213d0094c410404e7358dc477eb3
Sebastian F <sebpfave2@gmail.com>
2021-07-02 16:59:01 -0400

    Implement multi component import

--- src/components/PortfolioSite.js
+++ src/components/PortfolioSite.js
@@ -1,12 +1,31 @@
 import { useState } from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import { About, Portfolio, Contact, Skills } from './pages';
 
 export default function PortfolioSite() {
+  const [page, setPage] = useState('About');
+  const renderContent = () => {
+    switch (page) {
+      case 'About':
+        return <About />;
+      case 'Portfolio':
+        return <Portfolio />;
+      case 'Contact':
+        return <Contact />;
+      case 'Skills':
+        return <Skills />;
+
+      default:
+        break;
+    }
+  };
+
   return (
     <div>
       <Header />
-      <div>Sebastian Portfolio Site</div>
+      <About />
+      <Portfolio />
       <Footer />
     </div>
   );
---
+++ src/components/pages/index.js
@@ -0,0 +1,6 @@
+import About from './About';
+import Portfolio from './Portfolio';
+import Contact from './Contact';
+import Skills from './Skills';
+
+export { About, Portfolio, Contact, Skills };
