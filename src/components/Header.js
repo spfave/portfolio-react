6398184076720d96f872bdf1135ecf0977a0c678
Sebastian F <sebpfave2@gmail.com>
2021-07-03 19:07:21 -0400

    Implement mobie side menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,9 @@ import Nav from './Nav';
 
 export default function Header({ pages, page, handlePageChange }) {
   return (
-    <header className='flex justify-between items-center py-4 px-8 bg-gray-700'>
+    <header className='flex justify-between items-center p-4 bg-gray-700'>
       <a href='#home' onClick={() => handlePageChange(pages[0])}>
-        <h1 className='text-3xl text-gray-100 hover:text-gray-300 font-bold'>
+        <h1 className='text-3xl text-gray-100 hover:text-gray-300 font-bold trans-ease-in'>
           Sebastian Favè
         </h1>
       </a>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,43 @@
+import { MdMenu, MdClose } from 'react-icons/md';
+
 export default function Nav({ pages, page, handlePageChange }) {
   return (
-    <nav className='flex space-x-4'>
-      {pages.map((pageName, index) => (
-        <NavLink
-          key={index}
-          pageName={pageName}
-          handlePageChange={handlePageChange}
-        />
-      ))}
+    <nav className='flex flex-col items-end'>
+      <div>
+        {/* Visible links on medium+ screen */}
+        <div className='hidden md:flex items-center space-x-2'>
+          {pages.map((pageName, index) => (
+            <NavLink
+              key={index}
+              pageName={pageName}
+              handlePageChange={handlePageChange}
+            />
+          ))}
+        </div>
+
+        {/* Side Menu button on medium- screen */}
+        <div className='md:hidden'>
+          <button className='text-gray-200 hover:text-gray-100 hover:bg-gray-600 p-2 rounded trans-ease-in'>
+            <MdMenu size='30px' />
+          </button>
+        </div>
+      </div>
+
+      {/* Side menu on medium- screen */}
+      <div className='side-menu absolute right-0 inset-y-0 md:hidden flex flex-col items-end w-48 space-y-2  p-4 bg-gray-600 transform translate-x-full transition duration-200 ease-in-out'>
+        <button className='text-gray-200 hover:text-gray-100 hover:bg-gray-500 p-2 rounded'>
+          <MdClose size='30px' />
+        </button>
+        <div className='flex flex-col w-full space-y-2'>
+          {pages.map((pageName, index) => (
+            <NavLinkSide
+              key={index}
+              pageName={pageName}
+              handlePageChange={handlePageChange}
+            />
+          ))}
+        </div>
+      </div>
     </nav>
   );
 }
@@ -17,7 +47,19 @@ function NavLink({ pageName, handlePageChange }) {
     <a
       href={`#${pageName.toLowerCase()}`}
       onClick={() => handlePageChange(pageName)}
-      className='text-lg text-gray-300 hover:text-gray-200'
+      className='text-lg text-gray-300 hover:text-gray-200 hover:bg-gray-600 px-4 py-2 rounded trans-ease-in'
+    >
+      {pageName}
+    </a>
+  );
+}
+
+function NavLinkSide({ pageName, handlePageChange }) {
+  return (
+    <a
+      href={`#${pageName.toLowerCase()}`}
+      onClick={() => handlePageChange(pageName)}
+      className='py-2 px-4 text-lg text-gray-200 hover:text-gray-100 hover:bg-gray-500 rounded trans-ease-in'
     >
       {pageName}
     </a>
