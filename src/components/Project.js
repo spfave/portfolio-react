9dd36c9be21322e1ba769c0372984763f4d611a9
Sebastian F <sebpfave2@gmail.com>
2021-07-05 17:11:58 -0400

    Create Project card component

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,3 +1,40 @@
-export default function Project() {
-  return <div>Project</div>;
+import { FaCode } from 'react-icons/fa';
+
+export default function Project({ data }) {
+  return (
+    <div className='rounded-md overflow-hidden shadow-md bg-grey-50'>
+      <div className='flex flex-col h-full'>
+        <div className=''>
+          <img
+            className='object-cover object-center object-top w-full h-80'
+            src={data.image}
+            alt={data.imageAlt}
+          />
+        </div>
+        <div className='flex-grow p-4'>
+          <p className='font-bold mb-4'>{data.title}</p>
+          <ul className='list-disc list-outside ml-8'>
+            {data.notes.map((note, index) => (
+              <li key={index}>{note}</li>
+            ))}
+          </ul>
+        </div>
+        <div className='flex space-x-4 p-4'>
+          <a
+            className='inline-block w-1/2 px-4 py-2 border-2 rounded-md border-gray-600 hover:border-gray-500 bg-gray-600 hover:bg-gray-500 text-gray-200 hover:text-gray-100 text-center font-bold trans-ease-in'
+            href={data.link}
+          >
+            Live App
+          </a>
+          <a
+            className='inline-block w-1/2 px-4 py-2 border-2 rounded-md border-gray-300 hover:border-gray-400 hover:bg-gray-200 text-gray-500 hover:text-gray-600 text-center font-bold trans-ease-in'
+            href={data.source}
+          >
+            <FaCode className='inline' /> View Source
+          </a>
+        </div>
+        {/* tags */}
+      </div>
+    </div>
+  );
 }
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,3 +1,12 @@
+import Project from '../Project';
+import { projects } from '../../assets/data/portfolioData';
+
 export default function Portfolio() {
-  return <div>Portfolio</div>;
+  return (
+    <section className='grid lg:grid-cols-2 gap-8 p-8 bg-gray-100'>
+      {projects.map((project, index) => (
+        <Project key={index} data={project} />
+      ))}
+    </section>
+  );
 }
