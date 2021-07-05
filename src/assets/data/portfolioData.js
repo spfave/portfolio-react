1ea4624a92744c04dacaa43f0ec089c82b2223ff
Sebastian F <sebpfave2@gmail.com>
2021-07-05 17:02:19 -0400

    Update project demo data

--- src/assets/data/portfolioData.js
+++ src/assets/data/portfolioData.js
@@ -1,3 +1,4 @@
+import { projectImages } from '../img/projects';
 import { FaLinkedin, FaGithub, FaEnvelope } from 'react-icons/fa';
 
 export const pages = ['About', 'Portfolio', 'Contact', 'Skills'];
@@ -11,8 +12,23 @@ export const projects = [
       'App uses server-side HTML rendering with handlebars.js for the front end views, Node.js with express.js for back end routes and controller logic, and MySQL with Sequelize ORM for data persistence and CRUD operations',
       'User authentication and route protection implemented using passport.js using a local strategy',
     ],
+    image: projectImages.imgHikeTrackerApp,
+    imageAlt: 'Screen shot of hike tracker web application',
     link: 'https://gwu-hiker-tracker.herokuapp.com/',
     source: 'https://github.com/spfave/hike-tracker',
+  },
+  {
+    title: 'Tech Blog CMS',
+    notes: [
+      'Full stack web application following the model view controller design paradigm',
+      'Express, Node.js, MySQL, and Sequelize (ORM) for server and database functionality',
+      'Handlebars.js for server-side HTML view templates and rendering',
+      'Styled using themed Bootstrap file from bootswatch.com',
+    ],
+    image: projectImages.imgTechBlogApp,
+    imageAlt: 'Screen shot of tech blog application',
+    link: 'https://spf-tech-blog.herokuapp.com/',
+    source: 'https://github.com/spfave/blog-site-mvc',
   },
 ];
 
---
+++ src/assets/img/projects/index.js
@@ -0,0 +1,4 @@
+import imgHikeTrackerApp from './hiketracker_home.png';
+import imgTechBlogApp from './blog-create-post.png';
+
+export const projectImages = { imgHikeTrackerApp, imgTechBlogApp };
