2188139890d46eaeecdf25f6a40752be0e03b4dc
Sebastian F <sebpfave2@gmail.com>
2021-07-05 17:49:59 -0400

    Add all project image imports

--- src/assets/img/projects/index.js
+++ src/assets/img/projects/index.js
@@ -1,4 +1,15 @@
-import imgHikeTrackerApp from './hiketracker_home.png';
-import imgTechBlogApp from './blog-create-post.png';
+import techBlogApp from './blog-create-post.png';
+import expenseTrackerApp from './expense-home.png';
+import fitnessDashboard from './fitness-dashboard.png';
+import foodFitnessHome from './foodfitness_home.png';
+import hikeTrackerApp from './hiketracker_home.png';
+import weatherDashboard from './weather_dashboard.png';
 
-export const projectImages = { imgHikeTrackerApp, imgTechBlogApp };
+export const projectImages = {
+  techBlogApp,
+  expenseTrackerApp,
+  fitnessDashboard,
+  foodFitnessHome,
+  hikeTrackerApp,
+  weatherDashboard,
+};
