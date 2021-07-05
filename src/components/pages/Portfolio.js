45c1719e2da615254450e4ba08f86530ea6e69a0
Sebastian F <sebpfave2@gmail.com>
2021-07-05 18:57:37 -0400

    Edit Portfolio page to full width

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -3,7 +3,7 @@ import { projects } from '../../assets/data/portfolioData';
 
 export default function Portfolio() {
   return (
-    <section className='p-8 bg-gray-100'>
+    <section className='w-full p-8 bg-gray-100'>
       <h2 className='py-4 text-center text-gray-600 text-2xl font-bold uppercase '>
         Selected Works
       </h2>
