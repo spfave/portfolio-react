9ef3247740f7c0f42825275fb89740d3a04d9e1a
Sebastian F <sebpfave2@gmail.com>
2021-07-07 21:17:59 -0400

    Enable resume view from Skills page

--- src/components/pages/Skills.js
+++ src/components/pages/Skills.js
@@ -1,3 +1,16 @@
 export default function Skills() {
-  return <div>Skills</div>;
+  return (
+    <section className='w-full p-8 bg-gray-100'>
+      <h2 className='py-4 text-center text-gray-600 text-2xl font-bold uppercase '>
+        Web Development Experience
+      </h2>
+      <div className='flex flex-col items-center py-4'>
+        <div className='inline-block px-4 py-2 border-2 rounded-md border-gray-600 hover:border-gray-500 bg-gray-600 hover:bg-gray-500 text-gray-200 hover:text-gray-100 text-center font-bold trans-ease-in'>
+          <a href='./assets/docs/sebastian-fave-resume.pdf' target='_blank'>
+            Download Résumé
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 }
