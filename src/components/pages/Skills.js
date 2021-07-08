9dc32362b1ed681f003889fbdc6106a3b2391e0e
Sebastian F <sebpfave2@gmail.com>
2021-07-07 22:39:13 -0400

    Use skill data in Skills page

--- src/components/pages/Skills.js
+++ src/components/pages/Skills.js
@@ -1,14 +1,16 @@
+import { skills } from '../../assets/data/portfolioData';
+
 export default function Skills() {
   return (
     <section className='w-full p-8 bg-gray-100'>
       <h2 className='py-4 text-center text-gray-600 text-2xl font-bold uppercase '>
         Development Tech Stack
       </h2>
-      {/* <div className='flex flex-col space-y-8 items-center py-4'> */}
-      <div className='grid grid-cols-3 gap-8 py-8'>
-        <SkillCard title='Front End' notes={[1, 2, 3, 4]} />
-        <SkillCard title='Back End' notes={[1, 2, 3, 4]} />
-        <SkillCard title='Dev Tools' notes={[1, 2, 3, 4]} />
+
+      <div className='grid md:grid-cols-3 gap-8 py-8'>
+        <SkillCard data={skills.frontEnd} />
+        <SkillCard data={skills.backEnd} />
+        <SkillCard data={skills.devTools} />
       </div>
 
       <div className='flex justify-center mb-8'>
@@ -18,22 +20,23 @@ export default function Skills() {
           </a>
         </div>
       </div>
-      {/* </div> */}
     </section>
   );
 }
 
-function SkillCard({ title, notes }) {
+function SkillCard({ data }) {
   return (
     <div className='rounded-md overflow-hidden shadow-md bg-grey-50'>
       <div className='flex flex-col h-full'>
         <div className='bg-gray-600 px-4 py-3'>
-          <h4 className='text-gray-200 font-bold text-lg'>{title}</h4>
+          <h4 className='text-gray-200 font-bold text-lg'>{data.title}</h4>
         </div>
-        <div className='px-4 py-2'>
-          <ul>
-            {notes.map((note, index) => (
-              <li key={index}>{note}</li>
+        <div className='px-4 py-3'>
+          <ul className='list-disc list-outside ml-8'>
+            {data.notes.map((note, index) => (
+              <li className='my-1' key={index}>
+                {note}
+              </li>
             ))}
           </ul>
         </div>
